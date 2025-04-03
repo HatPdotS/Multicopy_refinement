b4cf3b23981201b94e8a479b51387ee354e241b1
HatPdotS <seidel_h@ra-l-005.psi.ch>
2025-04-03 15:00:59 +0200

    added tests

---
+++ tests/test_refinement.py
@@ -0,0 +1,25 @@
+import multicopy_refinement.refinement as refinement
+import multicopy_refinement.restraints_handler as restraints_handler
+import multicopy_refinement.Model as Model
+import multicopy_refinement.io as io
+
+
+cif_path = '/das/work/p17/p17490/Peter/Library/multicopy_refinement/test_data/Merged_restraints_all_opened.cif'
+
+restraints = restraints_handler.restraints(cif_path)
+
+
+M = Model.model()
+M.load_pdb_from_file('/das/work/p17/p17490/Peter/Library/multicopy_refinement/test_data/test_all_perturbed.pdb')
+hkl = io.read_mtz('/das/work/p17/p17490/Peter/Library/multicopy_refinement/test_data/test_direct_summation.mtz')
+
+ref = refinement.Refinement(hkl,model=M,restraints=restraints)
+
+
+print(ref.calc_rfactor())
+
+ref.refine()
+
+print(ref.calc_rfactor())
+
+M.write_pdb('/das/work/p17/p17490/Peter/Library/multicopy_refinement/test_data/test_all_perturbed_refined.pdb')
